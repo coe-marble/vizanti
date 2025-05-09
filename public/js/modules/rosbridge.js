c20fe7ea8f442406a7c3b0ae0ae6d1e38b04cc18
MoffKalast <vid.rijavec@gmail.com>
2025-05-10 00:12:16 +0200

    Switched from werkzeug to waitress for static serve (#146)
    * switched from werkzeug to waitress for static server (rerun rosdep install)
    
    * waitress logger fix
    
    * rosdep for docker
    
    * topic order fix, sort at the source
    
    * sort fix

--- public/js/modules/rosbridge.js
+++ public/js/modules/rosbridge.js
@@ -102,31 +102,37 @@ class Rosbridge {
 	}
 
 	async get_all_topics() {
-		return new Promise(async (resolve) => {
-			this.topics_client.callService(new ROSLIB.ServiceRequest({}), function (result) {
+		return new Promise((resolve) => {
+			this.topics_client.callService(new ROSLIB.ServiceRequest({}), (result) => {
+				const combined = result.topics.map((t, i) => [t, result.types[i]]);
+				combined.sort((a, b) => a[0].localeCompare(b[0]));
+				result.topics = combined.map(([t]) => t);
+				result.types = combined.map(([, ty]) => ty);
 				resolve(result);
 			});
 		});
 	}
-
+	
 	async get_topics(requested_type) {
 		return new Promise(async (resolve) => {
 			this.topics_client.callService(new ROSLIB.ServiceRequest({}), function (result) {
-
+	
 				let topics = result.topics;
 				let types = result.types;
-
-				let matching = [];				
-				for(let i = 0; i < topics.length; i++){
-					if(types[i] == requested_type){
+	
+				let matching = [];
+				for (let i = 0; i < topics.length; i++) {
+					if (types[i] == requested_type) {
 						matching.push(topics[i]);
 					}
 				}
-
+					
+				matching.sort();
 				resolve(matching);
 			});
 		});
 	}
+	
 
 	async get_services(requested_type) {
 		return new Promise((resolve, reject) => {
