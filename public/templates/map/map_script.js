aa93a50106e1d7196d7afa290208a0cd6698833b
MoffKalast <vid.rijavec@gmail.com>
2025-07-07 22:02:00 +0200

    Robot model gallery, projection fixes (Noetic) (#156)
    * 3D transform for robot sprites
    
    * robot model gallery selection
    
    * additional robot sprites
    
    * robotmodel offset params, occupancygrid pitch roll rendering
    
    * swapped euler to quat projection for robotmodel and map
    
    * fixed markerarray projection rendering
    
    * datalist of known satelite tile providers
    
    * gridcells test script
    
    * fixed satelite and gridcells projection
    
    * path redraw for fixed frame change

--- public/templates/map/map_script.js
+++ public/templates/map/map_script.js
@@ -205,6 +205,7 @@ async function drawMap(){
 	if(!map_data)
 		return;
 
+	ctx.setTransform(1,0,0,1,0,0);
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.imageSmoothingEnabled = false;
 
@@ -235,15 +236,12 @@ async function drawMap(){
 		y: tf_pose.translation.y,
 	});
 
-	const yaw = tf_pose.rotation.toEuler().h;
+	const matrix = view.quaterionToProjectionMatrix(tf_pose.rotation);
 
-	ctx.save();
 	ctx.globalAlpha = opacitySlider.value;
-	ctx.translate(pos.x, pos.y);
+	ctx.setTransform(matrix[0], matrix[1], matrix[2], matrix[3], pos.x, pos.y); //sx,0,0,sy,px,py
 	ctx.scale(1.0, -1.0);
-	ctx.rotate(yaw);
 	ctx.drawImage(temp_canvas, 0, 0, map_width, map_height);
-	ctx.restore();
 }
 
 //Topic
